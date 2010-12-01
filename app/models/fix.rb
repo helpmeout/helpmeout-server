e7a0d69b0c677056b9f38749e97f33a91007a64d
Manuel Kallenbach <manuel.kallenbach@gmail.com>
2010-12-01 13:06:31 -0800

    strip backtrace before savin

--- app/models/fix.rb
+++ app/models/fix.rb
@@ -2,6 +2,7 @@ class Fix < ActiveRecord::Base
   CODE_LINE_WEIGHT = 1
   EXCEPTION_MESSAGE_WEIGHT = 3
   has_many :fix_files
+  before_save :strip_backtrace
 
   accepts_nested_attributes_for :fix_files, :reject_if => :all_blank
 
@@ -15,5 +16,9 @@ class Fix < ActiveRecord::Base
     fix_files.collect do |fix_file|
       fix_file.distance(code_line)
     end.max
+  end
+
+  def strip_backtrace
+    backtrace = backtrace.strip unless backtrace.blank?
   end
 end
